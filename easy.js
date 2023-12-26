a88ae86d41183554975882f1e09af36ea89a7370
agnes0304 <jiwoochoi0304@gmail.com>
2023-12-26 15:06:53 +0900

    merge sorted arr

--- easy.js
+++ easy.js
@@ -1,7 +1,19 @@
 // 88. Merge Sorted Array
 
-console.log(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3); // [1,2,2,3,5,6]
+console.log(
+  (nums1 = [1, 2, 3, 0, 0, 0]),
+  (m = 3),
+  (nums2 = [2, 5, 6]),
+  (n = 3)
+); // [1,2,2,3,5,6]
 
 var merge = function (nums1, m, nums2, n) {
-  
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (nums2[i] <= nums1[j]) {
+        nums1.splice(j, 0, nums2[i]);
+        break;
+      }
+    }
+  }
 };
